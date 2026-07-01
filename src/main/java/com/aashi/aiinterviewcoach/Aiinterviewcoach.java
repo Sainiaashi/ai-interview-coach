ddcb5104abde1781f601ec0362c21be1e59db5db
Sainiaashi <aashisaini27012006@gmail.com>
2026-07-01 15:48:48 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/aashi/aiinterviewcoach/Aiinterviewcoach.java
@@ -0,0 +1,13 @@
+package com.aashi.aiinterviewcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aiinterviewcoach {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aiinterviewcoach.class, args);
+	}
+
+}
